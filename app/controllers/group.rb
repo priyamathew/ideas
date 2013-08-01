8ebf674f10d73d5f79469fb3cb3fb4ec8d2e6cb5
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-31 22:04:38 -0500

    added dropdown to select user to delete and delete user assoc from group

--- app/controllers/group.rb
+++ app/controllers/group.rb
@@ -19,5 +19,13 @@ post '/group/add_member' do
   redirect "/group/#{@group.id}/home"
 end
 
+post '/group/:group_id/remove_member' do
+  @group = Group.find(params[:group_id])
+  @user = User.find(params[:user][:id].to_i)
+  @group.users.destroy(@user.id)
+
+  erb :group_home
+end
+
 # Group.where(admin: current_user.id).each do |group|
 
